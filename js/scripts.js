532a066b05fcb63e527ec28802edc9d76c5d4446
lkrahling <Krahling.lauren@gmail.com>
2024-11-14 18:15:48 -0500

    Delete favoriteFood
    Deleted code from previous exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
-alert('Hello world');
+let pokemonList = [
+    {
+        name: "Bulbasaur", 
+        height: 7, 
+        types: ["grass", "poison"]
+    },
 
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+    {
+        name: "Ivysaur", 
+        height: 1, 
+        types: ["grass", "poison"]
+    },
+
+    {
+        name: "Venusaur", 
+        height: 2, 
+        types: ["grass", "poison"]
+    },
+];
 
